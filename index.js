2154eeabbb6c9575e80ea50af40cf8c0e59cb9a0
Brad Smith <brad@smithwebtek.com>
2019-05-05 08:01:07 -0400

    Remove comments, add semicolons;
    Remove comment

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 window.onload = () => {
-  // console.log('here is the event:', event);
-  // console.log('page is fully loaded');
-
   function listenForHoverOnMain() {
     let mainDiv = document.querySelector('.main')
     mainDiv.addEventListener('mouseover', (event) => {
-      // console.log('we hovered');
       event.preventDefault();
       mainDiv.style.backgroundColor = 'red';
-      // console.log('background turned red');
-    })
+    });
   }
 
   function turnGreen() {
@@ -21,4 +16,4 @@ window.onload = () => {
   }
   listenForHoverOnMain();
   turnGreen();
-}
+};
